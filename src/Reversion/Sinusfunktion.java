329ee78a3370f8f91c869ce30f6d3cd4ee5c902d
EduardDege <eduard.dege96@gmail.com>
2016-10-19 19:33:36 +0200

    Update Sinusfunktion.java

--- src/Reversion/Sinusfunktion.java
+++ src/Reversion/Sinusfunktion.java
@@ -7,6 +7,7 @@ import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
@@ -28,22 +29,27 @@ public class Sinusfunktion extends Application {
 	
 	
 	double z0;
+	
 	static double z1 = 8;  // Amplitude
 	double z2 = 0;
+	double z3 = 1.99;
 	int i = 0;
 	
 	
 	public void sinus(double f) {
 		if(i > 600)
 			return;
-		z0 = f * z1 - z2;
+		z0 = z3 * z1 - z2;
 		z2 = z1;
 		z1 = z0;
-		System.out.println(i + " " + (int)z0 ); 
+	//	System.out.println(i + " " + (int)z0 ); 
 		drawLine(i, (int)z0) ;
 		i++;
-		//f -= 0.02;
+		//z3=f;
+		//System.out.println(f);
+		//z3=f -= 0.001;
 		sinus(f);
+		
 	}
 	
 	public static void main(String[] args) {
@@ -65,48 +71,86 @@ public class Sinusfunktion extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
 		
-	    TextField a = new TextField ();
-		Slider slider= new Slider();
-		slider.setMin(0);
-		slider.setMax(100);
-		slider.setValue(40);
-		slider.setShowTickLabels(true);
-		slider.setShowTickMarks(true);
-		slider.setMajorTickUnit(50);
-		slider.setMinorTickCount(5);
-		slider.setBlockIncrement(10);
+	    TextField a = new TextField ("Amplitude");
+	    Button btn = new Button("-0.01");
+	    Button btn2= new Button("+0.01");
+		btn.setOnAction(new EventHandler<ActionEvent>(){
+			public void handle(ActionEvent event){
+				System.out.println(z3);
+				z3-=0.01;
+				System.out.println(z3);
+				
+			b.getChildren().clear();
+			
+			i=0;
+			cursorX = 0;
+			cursorY = 200;
+			
+			Rectangle c = new Rectangle(600, 400, Color.BLACK);			
+		    b.getChildren().add(c);
+		    b.setLeft(a);
+			b.setBottom(btn);
+			b.setRight(btn2);
+			sinus(1.99);
+			}
+			});
+		
+		btn2.setOnAction(new EventHandler<ActionEvent>(){
+			public void handle(ActionEvent event){
+				System.out.println(z3);
+				z3+=0.01;
+				System.out.println(z3);
+				
+			b.getChildren().clear();
+			
+			i=0;
+			cursorX = 0;
+			cursorY = 200;
+			
+			Rectangle c = new Rectangle(600, 400, Color.BLACK);			
+		    b.getChildren().add(c);
+		    b.setLeft(a);
+			b.setRight(btn2);
+			b.setBottom(btn);
+			sinus(1.99);
+			}
+			});
 		a.setOnAction(new EventHandler<ActionEvent>(){
 			public void handle(ActionEvent event){
-				System.out.println(z1);
+				
 				s = a.getText();
 				z1= Double.parseDouble(s);
 				a.setText("");
-				System.out.println(z1);
 				b.getChildren().clear();
 				z2=0;
 				i=0;
 				cursorX = 0;
 				cursorY = 200;
+				
+				Rectangle c = new Rectangle(600, 400, Color.BLACK);			
+			    b.getChildren().add(c);
+			    b.setLeft(a);
+				b.setBottom(btn);
+				b.setRight(btn2);
 				sinus(1.99);
-             				
-			
+				
 			}
 		});
+		
+	    
+		
 		b = new BorderPane();
 		Scene scene = new Scene(b, 600, 400);
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Scribble");
 		
 		
-		Rectangle panel = new Rectangle(600, 400, Color.WHITESMOKE);
+		Rectangle panel = new Rectangle(600, 400, Color.BLACK);
 		b.getChildren().add(panel);
-		//slider.setLayoutX(420);
-		//slider.setLayoutY(350);
-		//root.getChildren().add(slider);
-		//b.Pos.Top_Center(a);
-		//b.setBottom(slider);
+		
 		b.setLeft(a);
-		b.setRight(slider);
+		b.setRight(btn2);
+		b.setBottom(btn);
 		primaryStage.show();		
 	    
 		sinus(1.99);
